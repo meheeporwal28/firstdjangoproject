c380ed5a6f42518a2b96066fccc279e925540b0d
Mehee <meheeporwal28@gmail.com>
2023-05-18 22:13:16 +0530

    Project added to github repository

---
+++ textUtils/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('members/', views.members, name='members'),
+    # path('', views.index, name='index'),
+    # path('home', views.home, name='home'),
+    # path('about', views.about, name='about'),
+    # path('wishlist', views.wishlist, name='wishlist'),
+    # path('addtocart', views.addtocart, name='addtocart'),
+    # path('buynow', views.buynow, name='buynow'),
+
+    path('', views.index, name='index'),
+    path('analyze', views.analyze, name='analyze')
+]
